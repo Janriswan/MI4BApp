e28baddcfdd6c43e13b343451d76c87f6a0f6e6c
Janriswan <riswangaringging40@gmail.com>
2023-07-05 00:04:24 +0700

    Update index.php

--- app/Views/film/index.php
+++ app/Views/film/index.php
@@ -45,8 +45,8 @@ echo $this->section("content"); ?>
                             <?= $film["duration"] ?>
                         </td>
                         <td>
-                        <a href="/film/update/<?= $film["id"]; ?>" class="btn btn-success">Update</a>
-                        <a href="" class="btn btn-danger">Delete</a>
+                        <a href="/film/update/<?= encryptUrl($film["id"]); ?>" class="btn btn-success">Update</a>
+                        <a class="btn btn-danger" onclick="return confirmDelete()">Delete</a>
                         </td>
                     </tr>
                 <?php endforeach; ?>
@@ -64,5 +64,26 @@ echo $this->section("content"); ?>
 
   </div>
 </div>
+
+<script>
+    function confirmDelete() {
+        swal({
+                title: "Apakah Anda yakin?",
+                text: "setelah dihapus! data anda akan benar-benar hilang!",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            })
+            .then((willDelete) => {
+                if (willDelete) {
+
+                    window.location.href = "/film/destroy/<?= encryptUrl($film['id']) ?>";
+
+                } else {
+                    swal("Data tidak jadi dihapus!");
+                }
+            });
+    }
+</script>
 
 <?= $this->endSection();?>
